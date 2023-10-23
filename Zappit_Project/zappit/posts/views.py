9d73cabcddd1708955f6cd289da3b72c54a457b2
SaimiAh <ah.saim786@gmail.com>
2023-10-23 16:31:38 +0500

    Views Are Updated for better functionality.

--- Zappit_Project/zappit/posts/views.py
+++ Zappit_Project/zappit/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics,permissions,mixins
+from rest_framework import generics,permissions,mixins,status
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .serializers import PostSerializer,VoteSerializer
 from .models import Post,Vote
@@ -13,7 +14,7 @@ class PostList(generics.ListCreateAPIView):
         serializer.save(poster=self.request.user)
 
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -26,3 +27,10 @@ class VoteCreate(generics.CreateAPIView):
         if self.get_queryset().exists():
             raise ValidationError('You have already voted.')
         serializer.save(voter=self.request.user, post = Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You Never voted for it.')
