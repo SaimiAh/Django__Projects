c5067a72899974b8a08807cca433695d53714e50
SaimiAh <ah.saim786@gmail.com>
2023-10-24 18:10:39 +0500

    Udated The Code .

--- Hall_of _fame_videos/hofvidz/halls/models.py
+++ Hall_of _fame_videos/hofvidz/halls/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 class Hall(models.Model):
     title = models.CharField(max_length=255)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Video(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField()
