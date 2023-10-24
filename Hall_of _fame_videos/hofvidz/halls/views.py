cc877b152caee0ee2800e6304b7c2e19d564d13a
SaimiAh <ah.saim786@gmail.com>
2023-10-24 17:19:43 +0500

    Udated The Code .

--- Hall_of _fame_videos/hofvidz/halls/views.py
+++ Hall_of _fame_videos/hofvidz/halls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import Hall
 from django.contrib.auth.forms import UserCreationForm
 def home(request):
     return render(request, 'halls/home.html')
@@ -9,3 +10,10 @@ class SignUp(generic.CreateView):
     form_class  = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registeration/signup.html'
+
+
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
